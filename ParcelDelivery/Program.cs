ba3e69fcd03a83b9666d8f67923a916a7964920e
gru97 <fatemeh.shahidani@gmail.com>
2020-01-31 21:44:56 +0330

    base project

---
+++ ParcelDelivery/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParcelDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
